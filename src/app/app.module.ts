7407f3fd3880b305b3956724be225b35b89924e7
Maureen <murimimaureen69@gmail.com>
2020-06-18 23:23:21 +0300

    Add ngFor loop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     HeaderComponent,
-    FooterComponent
+
   ],
   imports: [
     BrowserModule,
