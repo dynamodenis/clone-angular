1c1cb62443c9033bc7d4922d41db908224d81aa8
Maureen <murimimaureen69@gmail.com>
2020-06-21 01:56:04 +0300

    Add input and output to question

--- src/app/students/quiz-detail/question/question.component.ts
+++ src/app/students/quiz-detail/question/question.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-question',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionComponent implements OnInit {
 
+  @Input()
+  question;
+
+  @Input()
+  selectedAnswer;
+
+  @Output()
+  selectAnswer = new EventEmitter<any>();
+
+  callParent(id: number) {
+    this.selectAnswer.next(id);
+  }
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.question);
   }
 
 }
