94288eca2e436219928147ceee9086b8bb3064d7
Brenda-M <brendanjuguna1@gmail.com>
2020-06-22 18:23:34 +0300

    add form input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { EndpointsService } from './services/endpoints.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,9 @@ import { QuiztestComponent } from './components/quiztest/quiztest.component';
 import { QuestionComponent } from './components/question/question.component';
 import { AnswerComponent } from './components/question/answer/answer.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 @NgModule({
   declarations: [
@@ -20,10 +24,14 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
     QuestionComponent,
     AnswerComponent,
     UserProfileComponent,
+    AuthComponent,
+    RegisterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     EndpointsService,
