87fcc15de0c224dce92094eef6e4ca1018e983bd
Brenda-M <brendanjuguna1@gmail.com>
2020-06-27 01:17:58 +0300

    modify login function

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private _authUrl = 'http://127.0.0.1:8000/api/auth/'
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
+
 
   constructor(
     private http: HttpClient,
     private router: Router
-  ) { }
+  ) { 
+    this.currentUserSubject = new BehaviorSubject<any>(localStorage.getItem('Token'));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
   registerMentor(user){
     return this.http.post<any>(this._authUrl + 'mentor/register/' , user)
@@ -21,8 +29,20 @@ export class AuthService {
     return this.http.post<any>(this._authUrl + 'student/register/', user)
   }
 
-  login(user){
-    return this.http.post<any>(this._authUrl +'login/', user)
+  // login(user){
+  //   return this.http.post<any>(this._authUrl +'login/', user)
+  // }
+  public get currentUserValue(){
+    return this.currentUserSubject.value;
+  }
+  
+  login(body){
+    return this.http.post<any>(this._authUrl +'login/', body).pipe(map(userInfo => {
+      let token =  userInfo.user.token
+      localStorage.setItem('Token', token);
+      this.currentUserSubject.next(userInfo);
+      return userInfo
+    }));
   }
 
   loggedIn(){
