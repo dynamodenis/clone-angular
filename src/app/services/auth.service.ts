e00e9e852f51274e3d336633084087d7c7f3aae5
dynamodenis <dmbugua66@gmail.com>
2020-06-30 11:40:04 +0300

    user profile linked

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
     return this.http.post<any>(this._authUrl + 'login/', user,)
   }
   loggedIn(){
-    return !!localStorage.getItem('Bearer')
+    return !!localStorage.getItem('Token')
   }
   logoutUser(){
     localStorage.removeItem('Bearer')
