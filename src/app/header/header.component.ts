03d77feccc60172530ab4fd2d3922c66dfa887f2
Brenda-M <brendanjuguna1@gmail.com>
2020-07-01 19:16:20 +0300

    style navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { EndpointsService } from '../services/endpoints.service';
 import { HttpClient } from '@angular/common/http';
+import * as $ from 'jquery';
+
 
 
 @Component({
@@ -17,18 +19,32 @@ export class HeaderComponent implements OnInit {
   constructor( public authService: AuthService, private router: Router, public endpoints: EndpointsService, private http:HttpClient) {
     this.authService.currentUser.subscribe(x => this.currentUser = x);
   
-   }
+  }
 
   logout(){
-    this.authService.logoutUser();
-    this.router.navigate(['/login'])
+      this.authService.logoutUser();
+      this.router.navigate(['/login'])
   }
 
   ngOnInit(): void {
-    this.authService.getUser().subscribe(res=>{
-      this.user=res;
-      console.log(this.user.user.is_mentor)
-  });
-}
+      this.authService.getUser().subscribe(res=>{
+        this.user=res;
+        console.log(this.user.user.is_mentor)
+    });
+
+
+    $(document).ready(
+      function() {
+        $('.menu-toggle').click(function() {
+          $('nav').toggleClass('active')
+        })
+        $('ul li').click(function() {
+          $(this).siblings().removeClass('active');
+          $(this).toggleClass('active');
+        })
+      }
+    )
+
+  }
 
 }
